3587a2c12aa6debd5a3cc736c89250d676bf89c0
Tom Vines <thedragonring.bod@gmail.com>
2020-01-14 12:16:47 +1030

    continue prep process

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+!(function() {
+  'use strict';
+  const id = '1f564xb3894ede3a46f94d',
+    files = [
+      '/car-cricket',
+      'assets/web-icon.png',
+      'assets/apple-icon.png',
+      'assets/sear.0.4.4.js',
+      'assets/scripts.js',
+      'assets/styles.css',
+      'index.html'
+    ],
+    cached = new Set(files);
+
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches
+        .open(`carcricket-cache${id}`)
+        .then(cache => cache.addAll(files))
+        .then(() => {
+          self.skipWaiting();
+        })
+    );
+  }),
+    self.addEventListener('activate', event => {
+      event.waitUntil(
+        caches.keys().then(async keys => {
+          for (const key of keys)
+            `carcricket-cache${id}` !== key && (await caches.delete(key));
+          self.clients.claim();
+        })
+      );
+    }),
+    self.addEventListener('fetch', event => {
+      if ('GET' !== event.request.method || event.request.headers.has('range'))
+        return;
+      const url = new URL(event.request.url);
+      url.protocol.startsWith('http') &&
+        ((url.hostname === self.location.hostname &&
+          url.port !== self.location.port) ||
+          (url.host === self.location.host && cached.has(url.pathname)
+            ? event.respondWith(caches.match(event.request))
+            : 'only-if-cached' !== event.request.cache &&
+              event.respondWith(
+                caches.open(`carcricket-offline${id}`).then(async cache => {
+                  try {
+                    const response = await fetch(event.request);
+                    cache.put(event.request, response.clone());
+                    return response;
+                  } catch (err) {
+                    const response = await cache.match(event.request);
+                    if (response) return response;
+                    throw err;
+                  }
+                })
+              )));
+    });
+})();
