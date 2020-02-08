cea7152e7471f98bd8583cc2682279f2dcaa8b1f
dragonwocky <thedragonring.bod@gmail.com>
2020-02-08 16:48:36 +1100

    add sear to cache

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,44 @@
 !(function() {
   'use strict';
   const ID = 'carcricket@dragonwocky',
-    ERROR = '/assets/error.html',
+    ERROR = 'assets/error.html',
     AUTOCACHE = [
       ERROR,
-      '/',
-      '/index.html',
-      '/assets/web-icon.png',
-      '/assets/apple-icon.png',
-      '/assets/scripts.js',
-      '/assets/styles.css'
+      '.',
+      'index.html',
+      'assets/web-icon.png',
+      'assets/apple-icon.png',
+      'assets/styles.css',
+      'assets/scripts.js',
+      'assets/sear.0.5.3.min.js'
     ];
+
+  /**
+   * FOR CACHING EXTERNAL RESOURCES
+   * - works, but not on initial load
+   */
+
+  // const EXTERNAL = [['external/sear.0.5.3.min.js', 'https://dragonwocky.me/sear/dist/sear.0.5.3.js']];
+  //
+  // in 'install':
+  // for (let i = 0; i < EXTERNAL.length; i++) {
+  //   const response = await fetch(EXTERNAL[i][1]);
+  //   if (response.status === 200) cache.put(EXTERNAL[i][0], response.clone());
+  // }
+  //
+  // in 'fetch'
+  // for (let i = 0; i < EXTERNAL.length; i++)
+  //   if (file[0] === new URL(event.request.url).pathname.slice(1)) {
+  //     response = await fetch(EXTERNAL[i][1]);
+  //     break;
+  // }
 
   self.addEventListener('install', event => {
     event.waitUntil(
       caches
         .open(ID)
         .then(cache => cache.addAll(AUTOCACHE))
-        .then(() => {
-          self.skipWaiting();
-        })
+        .then(() => self.skipWaiting())
     );
   });
   self.addEventListener('activate', event => {
@@ -39,14 +58,16 @@
     event.respondWith(
       caches.open(ID).then(async cache => {
         try {
-          const response = await fetch(event.request);
-          cache.put(event.request, response.clone());
+          let req = event.request,
+            response = await fetch(event.request);
+          if (response.status !== 200)
+            (req = new Request(ERROR)), (response = await fetch(req));
+          cache.put(req, response.clone());
           return response;
         } catch (err) {
-          const response = await cache.match(event.request);
+          const response =
+            (await cache.match(event.request)) || (await cache.match(new Request(ERROR)));
           if (response) return response;
-          const error = await cache.match(new Request(ERROR));
-          if (error) return error;
           throw err;
         }
       })
